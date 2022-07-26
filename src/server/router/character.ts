2c9a8db4e993d214ab7e23f804b1c00ba13851ed
Jacksonmills <jacksonemills@gmail.com>
2022-07-25 21:42:10 -0500

    simplified schema, getAll characters query

--- src/server/router/character.ts
+++ src/server/router/character.ts
@@ -2,18 +2,6 @@ import { createRouter } from "./context";
 import { z } from "zod";
 
 export const characterRouter = createRouter()
-  .query("hello", {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    },
-  })
   .query("getAll", {
     async resolve({ ctx }) {
       return await ctx.prisma.character.findMany();
